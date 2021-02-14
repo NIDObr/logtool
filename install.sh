ce72156fd70d49dd1314b594accc89d1cd90105f
NidoBr <coqecoisa@gmail.com>
2021-02-14 08:47:53 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@
 	printf '%s\n' "ROOT only!"
 	exit
 }
+# Create the directories
+mkdir /usr/share/tcltk/logtool/
+mkdir /var/log/logtool/
 # Copy the necessary files to the p directories
-cp *.tcl /usr/share/tcltk/
-cp *.md /usr/share/tcltk/
+cp *.tcl /usr/share/tcltk/logtool/
+cp *.md /usr/share/tcltk/logtool/
 # Make it executable
 chmod +x /usr/share/tcltk/logtool.tcl
+chmod -R 777 /var/log/logtool
